dff6afd7116050defb8c780b583aef67575f6536
crazyyy <crazyyy@gmail.com>
2016-06-20 11:41:16 +0300

    head, foot, frontpage

--- wordpress/wp-content/themes/wp-ulaf/footer.php
+++ wordpress/wp-content/themes/wp-ulaf/footer.php
@@ -1,127 +1,41 @@
-      <footer>
-        <div class="container">
-          <div class="row">
+    <footer>
+      <div class="container">
+        <div class="row">
 
-            <div class="col-sm-6">
-              <p>© Ulaf 2016</p>
-            </div><!-- col-sm-6 -->
+          <div class="col-sm-6">
+            <p>© Ulaf 2016</p>
+          </div><!-- col-sm-6 -->
 
-            <div class="col-sm-6">
-              <ul class="list-inline social">
-                <li>
-                  <a href="mailto:info@ulafua.com" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/social/email.png" alt="ulaf"></a>
-                </li>
-                <li>
-                  <a href="https://www.facebook.com/ULAF-1701766046732430" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/social/facebook.png" alt="ulaf Facebook"></a>
-                </li>
-                </li>
-                <li>
-                  <a href="https://vk.com/public116822349" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/social/vkontakte.png" alt="ulaf VK"></a>
-                </li>
-                <li>
-                  <a href="https://twitter.com/ULAFCOOL" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/social/twitter.png" alt="ulaf Twitter"></a>
-                </li>
-                <li>
-                  <a href="https://www.youtube.com/channel/UCX-eSs3MKCWTAnm3rQJ7WJQ" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/social/youtube.png" alt="ulaf YouTube"></a>
-                </li>
-                <li>
-                  <a href="https://www.instagram.com/ULAFCOOL/" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/social/instagram.png" alt="ulaf Instagram"></a>
-                </li>
-              </ul>
-            </div><!-- col-sm-6 -->
+          <div class="col-sm-6">
+            <ul class="list-inline social">
+              <li>
+                <a href="mailto:info@ulafua.com" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/social/email.png" alt="ulaf"></a>
+              </li>
+              <li>
+                <a href="https://www.facebook.com/ULAF-1701766046732430" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/social/facebook.png" alt="ulaf Facebook"></a>
+              </li>
+              </li>
+              <li>
+                <a href="https://vk.com/public116822349" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/social/vkontakte.png" alt="ulaf VK"></a>
+              </li>
+              <li>
+                <a href="https://twitter.com/ULAFCOOL" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/social/twitter.png" alt="ulaf Twitter"></a>
+              </li>
+              <li>
+                <a href="https://www.youtube.com/channel/UCX-eSs3MKCWTAnm3rQJ7WJQ" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/social/youtube.png" alt="ulaf YouTube"></a>
+              </li>
+              <li>
+                <a href="https://www.instagram.com/ULAFCOOL/" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/social/instagram.png" alt="ulaf Instagram"></a>
+              </li>
+            </ul>
+          </div><!-- col-sm-6 -->
 
-          </div><!-- row -->
-        </div><!-- container -->
-      </footer>
-    </div><!-- wrapper -->
+        </div><!-- row -->
+      </div><!-- container -->
+    </footer>
+  </div><!-- wrapper -->
 
-
-
-  <link rel="stylesheet" href="http://www.owlcarousel.owlgraphic.com/assets/owlcarousel/assets/owl.carousel.min.css">
-  <link rel="stylesheet" href="http://www.owlcarousel.owlgraphic.com/assets/owlcarousel/assets/owl.theme.default.min.css">
-
-  <script src="http://www.owlcarousel.owlgraphic.com/assets/vendors/jquery.min.js"></script>
-  <script src="http://www.owlcarousel.owlgraphic.com/assets/owlcarousel/owl.carousel.js"></script>
-
-  <script src="http://www.jqueryscript.net/demo/jQuery-Plugin-For-Fixed-Table-Header-Footer-Columns-TableHeadFixer/tableHeadFixer.js"></script>
-  <script>
-    $(document).ready(function() {
-      $(".team-score").tableHeadFixer({"head" : false, "left" : 1});
-      $(".player-score-qb").tableHeadFixer({"head" : false, "left" : 1});
-      $(".player-score-wr").tableHeadFixer({"head" : false, "left" : 1});
-      $(".player-score-te").tableHeadFixer({"head" : false, "left" : 1});
-      $(".player-score-c").tableHeadFixer({"head" : false, "left" : 1});
-      $(".player-score-cb").tableHeadFixer({"head" : false, "left" : 1});
-      $(".player-score-de").tableHeadFixer({"head" : false, "left" : 1});
-      $(".player-score-dt").tableHeadFixer({"head" : false, "left" : 1});
-      $(".player-score-fb").tableHeadFixer({"head" : false, "left" : 1});
-      $(".player-score-fs").tableHeadFixer({"head" : false, "left" : 1});
-      $(".player-score-ilb").tableHeadFixer({"head" : false, "left" : 1});
-      $(".player-score-mlb").tableHeadFixer({"head" : false, "left" : 1});
-      $(".player-score-olb").tableHeadFixer({"head" : false, "left" : 1});
-      $(".player-score-k").tableHeadFixer({"head" : false, "left" : 1});
-      $(".player-score-p").tableHeadFixer({"head" : false, "left" : 1});
-      $(".player-score-rb").tableHeadFixer({"head" : false, "left" : 1});
-      $(".player-score-ss").tableHeadFixer({"head" : false, "left" : 1});
-    });
-  </script>
-
-  <script>
-  $('.owl-home-slide').owlCarousel({
-      loop:true,
-      margin:10,
-      nav:false,
-      items:1,
-      autoplay: true,
-      autoplayTimeout: 8000,
-  })
-  </script>
-
-  <script>
-  $('.owl-player-slide').owlCarousel({
-      loop:true,
-      margin:10,
-      nav:true,
-      navText:['&#x3c;','&#x3e;'],
-      items:1,
-      autoplay: true,
-      autoplayTimeout: 5000,
-  })
-  </script>
-
-  <script>
-  $('.owl-team-slide').owlCarousel({
-      loop:true,
-      margin:10,
-      nav:true,
-      navText:['&#x3c;','&#x3e;'],
-      items:1,
-      autoplay: true,
-      autoplayTimeout: 5000,
-  })
-  </script>
-  <script>
-  $('.owl-people-slide').owlCarousel({
-      loop:true,
-      margin:10,
-      nav:true,
-      navText:['&#x3c;','&#x3e;'],
-      items:1,
-      autoplay: true,
-      autoplayTimeout: 5000,
-  })
-  </script>
-  <script>
-  $('.owl-footer-slide').owlCarousel({
-      loop:true,
-      margin:10,
-      nav:false,
-      items:4,
-      autoplay: true,
-      autoplayTimeout: 5000,
-  })
-  </script>
-
+  <?php wp_footer(); ?>
 
   </body>
 </html>
--- wordpress/wp-content/themes/wp-ulaf/front-page.php
+++ wordpress/wp-content/themes/wp-ulaf/front-page.php
@@ -1,32 +1,36 @@
 <?php /* Template Name: Home Page */ get_header(); ?>
 
   <div class="owl-home-slide">
-                      <?php if( have_rows('slider') ): ?>
-                    <?php while( have_rows('slider') ): the_row();
-                      // vars
-                      $link = get_sub_field('slider_link');
-                      $link2 = get_sub_field('slider_link2');
-                      $image = get_sub_field('slider_image');
-                      $content = get_sub_field('slider_title');
-                      $description = get_sub_field('slider_description'); ?>
+    <?php if( have_rows('slider') ): ?>
+      <?php while( have_rows('slider') ): the_row();
+        // vars
+        $link = get_sub_field('slider_link');
+        $link2 = get_sub_field('slider_link2');
+        $image = get_sub_field('slider_image');
+        $content = get_sub_field('slider_title');
+        $description = get_sub_field('slider_description'); ?>
+
         <div class="item-slide">
           <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt'] ?>" />
           <h1 class="slider_title"><?php echo $content; ?></h1>
           <h5 class="descr"><?php echo $description; ?></h5>
-        <div class="slider_but">
-             <?php if( $link ): ?>
-            <a href="<?php echo $link; ?>" class="btn btn-primary">Присоединяйтесь</a>
+
+          <div class="slider_but">
+            <?php if( $link ): ?>
+              <a href="<?php echo $link; ?>" class="btn btn-primary">Присоединяйтесь</a>
             <?php endif; ?>
             <?php if( $link2 ): ?>
-            <a href="<?php echo $link2; ?>" class="btn second">Подробнее</a>
+              <a href="<?php echo $link2; ?>" class="btn second">Подробнее</a>
             <?php endif; ?>
-        </div>
-      </div><!-- item-slide -->
-                 <?php endwhile; ?>
-                  <?php endif; ?>
-    </div><!-- /.owl-home-slide -->
+          </div><!-- slider_but -->
+        </div><!-- item-slide -->
+      <?php endwhile; ?>
+    <?php endif; ?>
+  </div><!-- /.owl-home-slide -->
+
   <section class="section-news">
     <div class="container">
+
       <div class="row news_block">
         <h3 class="col-md-12">Последние Новости</h3>
         <!-- start news loop -->
@@ -44,7 +48,6 @@
           <?php endwhile; endif; ?>
         <?php wp_reset_query(); ?>
         <!-- end news loop -->
-
       </div><!-- news_block -->
 
       <div class="owl-footer-slide">
@@ -54,6 +57,7 @@
           </div><!-- sponsors-footer-images -->
         <?php endforeach; endif; ?>
       </div><!-- owl-footer-slide -->
+
     </div><!-- container -->
   </section><!-- section-news -->
 
